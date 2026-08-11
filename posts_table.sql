bb8b427e5040437c84abfc4112a3e2fe2a4858f9
Tiago Mendes <tiagomendes.2acc@gmail.com>
2026-08-11 22:09:29 +0100

    Wrote an up migration for the posts table

--- posts_table.sql
+++ posts_table.sql
@@ -16,3 +16,16 @@ Use data types that make the most sense given the column name. For ID columns, a
   author_id INTEGER,
   is_sponsored BOOLEAN
 );
+/*
+ 
+Assignment
+
+Write an up migration for the posts table that achieves the following:
+
+    The author_id column should be renamed to poster_id.
+    Add a new column named is_edited with a BOOLEAN type.
+    DROP the is_sponsored column.
+
+*/ALTER TABLE posts RENAME COLUMN author_id TO poster_id;
+ALTER TABLE posts ADD COLUMN is_edited BOOLEAN;
+ALTER TABLE posts DROP COLUMN is_sponsored;
