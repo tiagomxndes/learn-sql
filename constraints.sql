00e22864f4c060ebd4aae40f145b752c1acfb39d
Tiago Mendes <tiagomendes.2acc@gmail.com>
2026-08-11 22:16:51 +0100

    wrote a query to select all fields on all records in the transactions table

---
+++ constraints.sql
@@ -0,0 +1,7 @@
+/*
+Assignment
+We didn't force any constraints on our tables when we created them, and it has allowed for NULL entries to make their way into our table! Let's take a look at our transactions table to see what those NULL values look like.
+Write a query to SELECT all fields on all records in the transactions table.
+*/
+
+SELECT  * FROM  transactions;
