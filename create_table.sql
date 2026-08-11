2f0cfd2c47be6acabb2b3d432a04e71aeb3b2186
Tiago Mendes <tiagomendes.2acc@gmail.com>
2026-08-11 21:37:28 +0100

    renamed the table to users, in users renamed the tag column to username, added the password TEXT column

--- create_table.sql
+++ create_table.sql
@@ -22,8 +22,23 @@ Create the transactions table with the following fields:
     note – TEXT
     amount – REAL
 
-*/
- CREATE TABLE people(
+Assignment
+
+We need to make some changes to the people table! At the moment, we have these six columns (shown as rows, so we can display datatypes):
+CID 	NAME 	TYPE 	NOTNULL 	DFLT VALUE 	PK
+0 	id 	INTEGER 	0 		0
+1 	tag 	TEXT 	0 		0
+2 	name 	TEXT 	0 		0
+3 	age 	INTEGER 	0 		0
+4 	balance 	REAL 	0 		0
+5 	is_admin 	BOOLEAN 	0 		0
+
+    Rename the table to users.
+    In users, rename the tag column to username.
+    In users, add the password (TEXT) column.
+
+
+*/CREATE TABLE people(
   id INTEGER,
   tag TEXT,
   name TEXT,
@@ -31,7 +46,6 @@ Create the transactions table with the following fields:
   balance REAL,
   is_admin BOOLEAN
 );
-
 CREATE TABLE transactions(
   id INTEGER,
   recipient_id INTEGER,
@@ -39,3 +53,6 @@ CREATE TABLE transactions(
   note TEXT,
   amount REAL
 );
+ALTER TABLE people RENAME TO users;
+ALTER TABLE users RENAME COLUMN tag TO username;
+ALTER TABLE users ADD COLUMN password TEXT;
