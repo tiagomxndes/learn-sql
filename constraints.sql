4dd8fb14087703d993c055f88d8b41e182dfb2ea
Tiago Mendes <tiagomendes.2acc@gmail.com>
2026-08-12 17:48:07 +0100

    Created the users table using primary key

--- constraints.sql
+++ constraints.sql
@@ -5,3 +5,30 @@ Write a query to SELECT all fields on all records in the transactions table.
 */
 
 SELECT * FROM transactions;
+
+/*
+Assignment
+
+Thankfully all the tables we have created for CashPal up to this point have been for testing purposes! Now that we have a better understanding of constraints, let's rebuild our database with the proper constraints and tables.
+
+Create the users table with the following fields and constraints:
+
+    id – INTEGER, PRIMARY KEY
+    name – TEXT, NOT NULL
+    age – INTEGER, NOT NULL
+    country_code – TEXT, NOT NULL
+    username – TEXT, UNIQUE, NOT NULL
+    password – TEXT, NOT NULL
+    is_admin – BOOLEAN
+
+ */
+
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  age INTEGER NOT NULL,
+  country_code TEXT NOT NULL,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  is_admin BOOLEAN
+);
